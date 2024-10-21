ce1f837a8a353452338204892fc6d8dd0b2b2f52
Hetso <datacener23491248@gmail.com>
2024-10-21 10:49:36 +0500

    Пофиксил ошибки

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 	app.setGlobalPrefix('api')
 
 	app.enableCors({
-		origin: true,
+		origin: 'https://scratchpad-frontend-umber.vercel.app/',
 		credentials: true,
 		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 		allowedHeaders: 'Content-Type, Accept, Authorization',
